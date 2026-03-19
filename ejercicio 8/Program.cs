fa49671675f109374fee86a4d06bdbf3ede9f9cd
pedro mendez <prmendez222@gmail.com>
2026-03-18 21:15:07 -0600

    ejercicio 8 terminado

---
+++ ejercicio 8/Program.cs
@@ -0,0 +1,73 @@
+﻿List<Cuenta> cuentas = new List<Cuenta>();
+
+Console.Write("Ingrese la cantidad de cuentas: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.Clear();
+
+for (int i = 0; i < n; i++)
+{
+    Cuenta c = new Cuenta();
+
+    Console.WriteLine("Cuenta #" + (i + 1));
+
+    Console.Write("Titular: ");
+    c.Titular = Console.ReadLine();
+
+    Console.Write("Saldo inicial: ");
+    c.Saldo = double.Parse(Console.ReadLine());
+
+    cuentas.Add(c);
+
+    Console.Clear();
+}
+
+foreach (Cuenta c in cuentas)
+{
+    Console.WriteLine($"Cuenta de {c.Titular}");
+
+    Console.Write("Monto a depositar: ");
+    double deposito = double.Parse(Console.ReadLine());
+    c.Depositar(deposito);
+
+    Console.Write("Monto a retirar: ");
+    double retiro = double.Parse(Console.ReadLine());
+    c.Retirar(retiro);
+
+    Console.Clear();
+}
+
+Console.WriteLine("Saldos finales");
+
+foreach (Cuenta c in cuentas)
+{
+    c.Mostrar();
+}
+class Cuenta
+{
+    public string Titular;
+    public double Saldo;
+
+    public void Depositar(double monto)
+    {
+        Saldo += monto;
+    }
+
+    public void Retirar(double monto)
+    {
+        if (monto <= Saldo)
+        {
+            Saldo -= monto;
+        }
+        else
+        {
+            Console.WriteLine("Fondos insuficientes");
+        }
+    }
+
+    public void Mostrar()
+    {
+        Console.WriteLine($"Titular: {Titular} | Saldo: {Saldo}");
+        Console.WriteLine();
+    }
+}
